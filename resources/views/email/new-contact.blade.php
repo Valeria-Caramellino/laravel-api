eaec900ef57ecccdd3aa728c1e525bf8ddaddcf1
valeria caramellino <valeriacaramellino@gmail.com>
2023-07-21 17:52:20 +0200

    add model controller view table for mail

---
+++ resources/views/email/new-contact.blade.php
@@ -0,0 +1,5 @@
+<h1>Ciao Amministratore</h1>
+<p>Hai ricenuto una notifica</p>
+<p>Nome: {{$contactData->name}}</p>
+<p>Indirizzo Mail: {{$contactData->email}}</p>
+<p>Message: {{$contactData->message}}</p>
